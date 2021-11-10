61e25e91ab86a6ccea464fe859236812942969cf
Nicolas Olenev <cardinal2113@gmail.com>
2021-11-11 01:02:53 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function showVerticalMessage(str) {
+  let result = '';
+  if (str.length > 10) {
+    str = str.slice(0, 10);
+  }
+  if (str[0] === 'м') {
+    str = str[0].toUpperCase() + str.slice(1);
+  }
+  for (let char of str) {
+    result += char + '\n';
+  }
+  return result.trim();
+}
+
+console.log(showVerticalMessage('марафон'));
